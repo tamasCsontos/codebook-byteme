79f7f1e43d1c139ad5c17c0e54d16c6483a143b8
Balogh David <balogh.david1015@gmail.com>
2018-06-13 11:32:02 +0200

    queries initialized, template text

--- src/main/java/com/codecool/codebook/controller/IndexController.java
+++ src/main/java/com/codecool/codebook/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.codecool.codebook.controller;
 
 import com.codecool.codebook.config.TemplateEngineUtil;
+import com.codecool.codebook.sql.Queries;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,14 +11,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import com.codecool.codebook.sql.Queries;
 
 @WebServlet(urlPatterns = {"/"})
 public class IndexController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("students", Queries.getAllStudent());
         engine.process("index.html", context, resp.getWriter());
     }
 }
---
+++ src/main/java/com/codecool/codebook/sql/Queries.java
@@ -0,0 +1,20 @@
+package com.codecool.codebook.sql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class Queries {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("codebookPU");
+    private static EntityManager em = emf.createEntityManager();
+
+    public static List getAllStudent(){
+        Query query = em.createQuery("SELECT name from Student");
+
+        return query.getResultList();
+
+
+    }
+}
