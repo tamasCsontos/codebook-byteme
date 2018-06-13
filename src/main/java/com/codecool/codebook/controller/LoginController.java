62f8d5539f0b1d17d0f8ff3e292eae0c7ec58f3b
Csonti <csontostamas1991@gmail.com>
2018-06-13 14:37:13 +0200

    login controller and login Html

---
+++ src/main/java/com/codecool/codebook/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.codecool.codebook.controller;
+
+
+import com.codecool.codebook.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/login"})
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("login.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        String hashedPAssword = com.codecool.shop.dao.jdbcImplementation.Password.hashPassword(password);
+
+    }
+}
