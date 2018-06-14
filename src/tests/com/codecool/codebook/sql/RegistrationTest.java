a365006df5401294ef24db1d9fd59603a0086928
Andras Zibar <zibandris@gmail.com>
2018-06-14 13:37:15 +0200

    test structure unified

--- src/tests/com/codecool/codebook/sql/RegistrationTest.java
+++ src/tests/com/codecool/codebook/sql/RegistrationTest.java
@@ -1,9 +1,19 @@
+package com.codecool.codebook.sql;
+
 import com.codecool.codebook.model.Student;
 import com.codecool.codebook.sql.Queries;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationTest {
+
+    @Before
+    public void setEnvironment(){
+        Queries.setEnv("codebooktestPU");
+    }
 
     @Test
     public void newItemAdded(){
@@ -13,7 +23,6 @@ public class RegistrationTest {
         Queries.addNewStudent(testStudent);
         Integer newLength = Queries.getAllStudentInfo().size();
         Assert.assertEquals(necessaryLength, newLength);
-        Queries.deleteStudent("TEST@TEST");
     }
 
     @Test
@@ -22,6 +31,10 @@ public class RegistrationTest {
         Queries.addNewStudent(testStudent);
         Student addedStudent = Queries.getStudent("TEST@TEST");
         Assert.assertTrue(addedStudent.equals(testStudent));
+    }
+
+    @AfterEach
+    public void deleteTestStudent(){
         Queries.deleteStudent("TEST@TEST");
     }
 
