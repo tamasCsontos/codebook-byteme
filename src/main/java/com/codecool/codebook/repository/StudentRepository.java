189f96a4f754a56f8ac445ed15a8d2e8cde3f0bb
Oláh Tamás <tolah94@gmail.com>
2018-07-10 10:38:27 +0200

    Added repositories

---
+++ src/main/java/com/codecool/codebook/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.codecool.codebook.repository;
+
+import com.codecool.codebook.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
