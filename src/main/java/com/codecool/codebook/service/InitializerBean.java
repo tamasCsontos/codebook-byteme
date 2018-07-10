988294cf807dd5e534d404d9cce772e2dff2efc9
Oláh Tamás <tolah94@gmail.com>
2018-07-10 10:48:45 +0200

    Added initializet and SpringBoot main

---
+++ src/main/java/com/codecool/codebook/service/InitializerBean.java
@@ -0,0 +1,17 @@
+package com.codecool.codebook.service;
+
+import com.codecool.codebook.repository.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitializerBean {
+
+    public InitializerBean(ActualJobRepository actualJobRepository,
+                           KlassRepository klassRepository,
+                           MessageRepository messageRepository,
+                           SchoolRepository schoolRepository,
+                           StudentRepository studentRepository,
+                           WorkplaceRepository workplaceRepository) {
+        
+    }
+}
