f6f1ec3dd77afba8a1ef52f5d77a6f1ed75cdafb
Oláh Tamás <tolah94@gmail.com>
2018-07-11 16:40:51 +0200

    Message update in progress

---
+++ src/main/java/com/codecool/codebook/controller/MessageRestController.java
@@ -0,0 +1,52 @@
+package com.codecool.codebook.controller;
+
+import com.codecool.codebook.model.Message;
+import com.codecool.codebook.model.Student;
+import com.codecool.codebook.sql.Queries;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class MessageRestController {
+
+    @Autowired
+    HttpSession session;
+
+    @Autowired
+    Queries queries;
+
+    @GetMapping("/messageupdate")
+    public List<Map> showMessage(@RequestParam("id") String receiverId) throws JSONException {
+        if (session.getAttribute("userID") != null) {
+            Long senderId = (Long) session.getAttribute("userID");
+            List<Message> messages = queries.getAllMessageBetweenUsers(senderId, Long.valueOf(receiverId));
+            JSONObject messageJson = new JSONObject();
+            List<Map> mapsForMessage = new ArrayList<>();
+            for (Message message : messages) {
+                Map<String, String> messageMap = new HashMap<>();
+                Student sender = message.getSenderStudent();
+                String senderName = sender.getName();
+                String textMessage = message.getTextMessage();
+                messageMap.put("sender", senderName);
+                messageMap.put("text_message", textMessage);
+                mapsForMessage.add(messageMap);
+            }
+            messageJson.put("messages", mapsForMessage);
+            return mapsForMessage;
+        }
+        return null;
+    }
+}
