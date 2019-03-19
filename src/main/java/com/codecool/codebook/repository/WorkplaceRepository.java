5fec0c7d7937f1fcfda9c3c4349eada3435b600c
Csonti <csontostamas1991@gmail.com>
2019-03-19 13:55:35 +0100

    refactoring WorkplaceRepository for rest api

--- src/main/java/com/codecool/codebook/repository/WorkplaceRepository.java
+++ src/main/java/com/codecool/codebook/repository/WorkplaceRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WorkplaceRepository extends JpaRepository<Workplace, Long> {
 
     Workplace findByNameEquals(String name);
+
 }
